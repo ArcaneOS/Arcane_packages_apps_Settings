489164ed4a3a8277e7b568ebba8a48e4f4020958
dlwlrma123 <alexfinhart@gmail.com>
2021-11-10 11:48:46 +0800

    Revert "Settings: Icons: do not render circle mask for dashboard icons"
    This reverts commit bd7e9df759fe6f14ec0c004bfb0055a89b8df3d1.

--- src/com/android/settings/widget/OPAdaptiveIconShapeDrawable.java
+++ src/com/android/settings/widget/OPAdaptiveIconShapeDrawable.java
@@ -48,7 +48,7 @@ public class OPAdaptiveIconShapeDrawable extends ShapeDrawable {
         getPaint().setAntiAlias(true);
         getPaint().setStyle(Paint.Style.STROKE);
         getPaint().setStrokeWidth(3.0f);
-        getPaint().setColor(resources.getColor(R.color.opShapeIcon));
+        getPaint().setColor(resources.getColor(R.color.settingsHeaderColor));
         setShape(new PathShape(path, 100.0f, 100.0f));
     }
 }
